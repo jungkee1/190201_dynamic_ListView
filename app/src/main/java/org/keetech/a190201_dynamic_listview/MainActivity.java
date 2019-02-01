6ab5a4ea51d421919e02f9d0de94bdc5ffcd703b
coding in pain <45281008+jungkee1@users.noreply.github.com>
2019-02-01 17:52:53 +0900

    Initial commit

---
+++ app/src/main/java/org/keetech/a190201_dynamic_listview/MainActivity.java
@@ -0,0 +1,48 @@
+package org.keetech.a190201_dynamic_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArrayList<String> midList = new ArrayList<String>();
+        ListView list = findViewById(R.id.listView1);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, midList);
+        list.setAdapter(adapter);
+
+        final EditText edtItem = findViewById(R.id.edtItem);
+        Button btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                midList.add(edtItem.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                midList.remove(arg2);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+    }
+}
